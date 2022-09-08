e5b4346f1aa30204c19b2bb0d4f562f265878a49
Victor Jansen <victorfjansen@gmail.com>
2022-09-08 13:28:32 -0300

    feat(packages): adds packages for connection with database - sequelize ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './shared';
+
 @Module({
-    imports: [],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'findserverisrunning',
+            database: 'FIND_SERVER',
+            models: [User],
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
